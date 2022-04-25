acf3ddfe3928aed900b13b4280dadd2c52a719ed
inpatr <i.traub@posteo.de>
2022-04-25 16:56:11 +0200

    added endpoint but need testing

--- src/index.ts
+++ src/index.ts
@@ -96,6 +96,36 @@ app.get<GetUserType>(
   }
 );
 
+// ----- EVENTS ----- //
+
+app.get<{ Querystring: { limit: number } }>(
+  "/events",
+  {
+    schema: {
+      querystring: {
+        limit: Type.Number(),
+      },
+    },
+  },
+  async (request, reply) => {
+    const { limit } = request.query;
+    try {
+      const events = await prisma.booking.findMany({
+        where: {
+          private: false,
+        },
+        take: limit,
+        orderBy: {
+          start: "desc",
+        },
+      });
+      reply.status(200).send(events);
+    } catch (err) {
+      send500(reply);
+    }
+  }
+);
+
 const start = async () => {
   try {
     await app.listen(8000, "0.0.0.0");

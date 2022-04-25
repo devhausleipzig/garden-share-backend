8339992e962282616f84447b042bc7fb5000b5be
Dan McAtee <danielmcatee@me.com>
2022-04-25 12:20:22 +0200

    define schema

--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,86 @@
-import { Prisma, PrismaClient } from '@prisma/client'
-import { fastify, FastifyInstance, RouteOptions } from 'fastify'
-import { Static, Type } from '@sinclair/typebox'
-import fastifySwagger from 'fastify-swagger'
+import { Prisma, PrismaClient } from "@prisma/client";
+import { fastify, FastifyInstance, RouteOptions } from "fastify";
+import { Static, Type } from "@sinclair/typebox";
+import fastifySwagger from "fastify-swagger";
 
-const prisma = new PrismaClient()
-const app = fastify({ logger: true })
+const prisma = new PrismaClient();
+const app = fastify({ logger: true });
 
 // write utility for gathering tags from "component" folders
-const tags: Array<{ name: string, description: string}> = [];
+const tags: Array<{ name: string; description: string }> = [];
 
 app.register(fastifySwagger, {
-    exposeRoute: true,
-    routePrefix: '/docs',
-    swagger: {
-        info: { title: 'fastify-api', version: '0.1.0' },
-    },
-})
+  exposeRoute: true,
+  routePrefix: "/docs",
+  swagger: {
+    info: { title: "fastify-api", version: "0.1.0" },
+  },
+});
 
 // write utility for gathering routes from "component" folders
-const routes: Array<(fastify: FastifyInstance, options: RouteOptions) => void> = [];
-routes.forEach( (route) => {
-    app.register(route)
-} )
-
+const routes: Array<(fastify: FastifyInstance, options: RouteOptions) => void> =
+  [];
+routes.forEach((route) => {
+  app.register(route);
+});
 
 const CreatePostModel = Type.Object({
-    title: Type.String(),
-    content: Type.String()
-})
+  title: Type.String(),
+  content: Type.String(),
+});
 
 const CreateUserModel = Type.Object({
-    name: Type.String(),
-    email: Type.String({ format: 'email' }),
-    posts: Type.Array(CreatePostModel)
-})
+  name: Type.String(),
+  email: Type.String({ format: "email" }),
+  posts: Type.Array(CreatePostModel),
+});
 
-app.get('/', {}, async (request, reply) => reply.send('hello'))
+app.get("/", {}, async (request, reply) => {
+  reply.send("Hello");
+});
 
-app.post<{ Body: Static<typeof CreateUserModel> }>
-(
-    `/signup`,
-    { 
-        schema: {
-            body: CreateUserModel,
-            response: {
-                200: Type.String()
-            }
-        }
+app.post<{ Body: Static<typeof CreateUserModel> }>(
+  `/signup`,
+  {
+    schema: {
+      body: CreateUserModel,
+      response: {
+        200: Type.String(),
+      },
     },
-    async (request, reply) => {
-        const { name, email, posts } = request.body
+  },
+  async (request, reply) => {
+    const { name, email, posts } = request.body;
 
-        const postData = posts?.map((post: Prisma.PostCreateInput) => {
-            return { title: post?.title, content: post?.content }
-        })
+    // const postData = posts?.map((post: Prisma.PostCreateInput) => {
+    //   return { title: post?.title, content: post?.content };
+    // });
 
-        const result = await prisma.user.create({
-            data: {
-                name,
-                email,
-                posts: {
-                    create: postData,
-                },
-            },
-        })
+    // const result = await prisma.user.create({
+    //   data: {
+    //     name,
+    //     email,
+    //     posts: {
+    //       create: postData,
+    //     },
+    //   },
+    // });
 
-        reply.send(result.id)
-    }
-)
+    reply.send(JSON.stringify({ name, email, posts }));
+  }
+);
 
-app.get('/users', async (request, reply) => {
-    const users = await prisma.user.findMany()
-    reply.send(users)
-})
+app.get("/users", async (request, reply) => {
+  const users = await prisma.user.findMany();
+  reply.send(users);
+});
 
 const start = async () => {
-    try {
-        await app.listen(8000, '0.0.0.0')
-    } catch (err) {
-        app.log.error(err)
-        process.exit(1)
-    }
-}
-start()
+  try {
+    await app.listen(8000, "0.0.0.0");
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+start();
